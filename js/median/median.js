858db0720aa6fbf8b5c9ba980ac1040271aafa92
olback <git@olback.net>
2018-03-12 18:26:36 +0100

    formatting

--- js/median/median.js
+++ js/median/median.js
@@ -15,14 +15,14 @@ if (typeof prompt === 'function') {
  *	0. Javascript array method-chaning is beatuful!
  *	1. Split the string into an array, seperated by the defined delimiter.
  *	2. Convert everything in the array to numbers.
- *  3. Filter out the values that are NaN by comparing it with itself, because in Javascript, NaN !== NaN.
+ *	3. Filter out the values that are NaN by comparing it with itself, because in Javascript, NaN !== NaN.
  * 	4. Finaly sort the array, starting with the smallest number.
  */
 const arr = input.split(DELIMITER).map(x => Number(x)).filter(val => val === val).sort((a, b) => a - b);
 
 /* 
  *  If there are an even amount of numbers, print the avrige of the two in the middle,
- * 	else print the median.
+ *	else print the median.
  */
 if (arr.length % 2 === 0) {
 	console.log((arr[(arr.length / 2) - 1] + arr[(arr.length / 2)]) / 2);
